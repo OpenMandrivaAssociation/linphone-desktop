005bc969138af6faee1cea95c70c649795402ba6
mandian <mandian@users.noreply.github.com>
2022-12-17 01:48:19 +0000

    add check-update.sh

---
+++ check-update.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+#curl -s "https://github.com/BelledonneCommunications/linphone-desktop/tags" |grep "tag/" |sed -e 's,.*tag/,,;s,\".*,,;' |grep -E '^[0-9.]+$' |grep -v '[0-9][0-9][0-9][0-9]\.[0-9][0-9]\.[0-9][0-9]' |sort -V |tail -n1
+curl -s "https://gitlab.linphone.org/BC/public/linphone-desktop/-/tags" |grep "tags/" |sed -e 's,.*tags/,,;s,\".*,,;' |grep -E '^[0-9.]+$' |grep -v '[0-9][0-9][0-9][0-9]\.[0-9][0-9]\.[0-9][0-9]' |sort -V |tail -n1
+
